a65fa15c51f389cca3d17b192a50ed969a8cac44
jsantoy <Jorge_Santoyo@gap.com>
2016-05-19 14:13:26 -0700

    Added lombok to project

--- Server/src/main/java/com/swarm/Swarm.java
+++ Server/src/main/java/com/swarm/Swarm.java
@@ -2,9 +2,6 @@ package com.swarm;
 
 import java.util.HashMap;
 
-/**
- * Created by igorsorokin on 5/19/16.
- */
 public class Swarm {
     HashMap<String, User> users = new HashMap<String, User>();
 
--- Server/src/main/java/com/swarm/SwarmController.java
+++ Server/src/main/java/com/swarm/SwarmController.java
@@ -3,19 +3,14 @@ package com.swarm;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Created by igorsorokin on 5/19/16.
- */
+
 @RestController
 public class SwarmController {
 
     @RequestMapping("/")
-    public String test()
+    public String postDesign()
     {
         return "hi";
     }
 
-    /*
-
-     */
 }
