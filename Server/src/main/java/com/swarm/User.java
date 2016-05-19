370cd64a279c8d73418a6d8b73766747c750997c
Igor Sorokin <igorsorokin@Igors-MBP.gap.com>
2016-05-19 15:08:32 -0700

    update

--- Server/src/main/java/com/swarm/User.java
+++ Server/src/main/java/com/swarm/User.java
@@ -2,9 +2,23 @@ package com.swarm;
 
 import java.util.HashMap;
 
-/**
- * Created by igorsorokin on 5/19/16.
- */
 public class User {
-    
+    String userId;
+    HashMap<String, Design> designs = new HashMap<String, Design>();
+
+    String getUserId()
+    {
+        return userId;
+    }
+
+    public User(String userId)
+    {
+        this.userId = userId;
+    }
+
+    void addDesign(String designId, Boolean isLike)
+    {
+        Design newDesign = new Design(designId, isLike);
+        designs.put(designId, newDesign);
+    }
 }
