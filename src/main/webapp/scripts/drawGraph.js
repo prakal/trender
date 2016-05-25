89cca341f79d3dbc92b6edab8177085bbeaae263
jsantoy <Jorge_Santoyo@gap.com>
2016-05-24 20:04:08 -0700

    mock for presentation

--- src/main/webapp/scripts/drawGraph.js
+++ src/main/webapp/scripts/drawGraph.js
@@ -9,40 +9,56 @@ function drawGraph(myGraph, json, keepNodesOnTop,swarmList, resetJSONLinks) {
     graph.addInitialNodesAndLinks(json);
 
     keepNodesOnTop();
-    // callback for the changes in the network
-    $("#addNodes").on("click", function(){
-      function addRandomLink(){
+
+    function addRandomLink(){
         var randomNode1 = Math.floor(Math.random() * json.nodes.length);
         var randomNode2 = Math.floor(Math.random() * json.nodes.length);
         while (randomNode1 == randomNode2){
-          randomNode2 = Math.floor(Math.random() * swarmList.length);
+            randomNode2 = Math.floor(Math.random() * swarmList.length);
         }
         // console.log(randomNode1,randomNode2, json.nodes.length);
         swarmList.push([randomNode1,randomNode2]);
         resetJSONLinks(json, swarmList[swarmList.length-1], graph);
-      }
+    }
 
-      function changeRandomLink(){
+    function changeRandomLink(){
         var randomSwarm = Math.floor(Math.random() * swarmList.length);
         var randomTimes = 1 + Math.floor(Math.random() * 1.5);
         console.log(randomSwarm, swarmList[randomSwarm], randomTimes);
         for (var i = 0; i < randomTimes; i++){
-          var randomNode1 = Math.floor(Math.random() * json.nodes.length);
-          // console.log('randomNode1', randomNode1);
-          if (swarmList[randomSwarm].indexOf(randomNode1) === -1){
-            swarmList[randomSwarm].push(randomNode1);
-          }
+            var randomNode1 = Math.floor(Math.random() * json.nodes.length);
+            // console.log('randomNode1', randomNode1);
+            if (swarmList[randomSwarm].indexOf(randomNode1) === -1){
+                swarmList[randomSwarm].push(randomNode1);
+            }
         }
         resetJSONLinks(json, swarmList[randomSwarm], graph);
-      }
+    }
+
+    // callback for the changes in the network
+    $("#addNodes").on("click", function(){
+
 
       // var len = json.nodes.length++
        // graph.addNode(len++);
        // make random connection to newly added node x
        // swarmList.push([6,len-1]);
-       setTimedInterval(addRandomLink, 1500, 10000);
-       setTimedInterval(changeRandomLink, 2000, 10000);
+       //setTimedInterval(addRandomLink, 1500, 10000);
+       //setTimedInterval(changeRandomLink, 2000, 10000);
+
+        addRandomLink();
+        changeRandomLink();
 
        keepNodesOnTop();
+    });
+
+    var socket = new SockJS('http://localhost:8080/trender');
+    var stompClient = Stomp.over(socket);
+    stompClient.connect({}, function(frame) {
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/swarm/update', function(greeting){
+            addRandomLink();
+            changeRandomLink();
+        });
     });
 }
